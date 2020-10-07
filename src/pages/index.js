d94e2e05a5f3ca45bc7b615be26c005b8ade67ed
Przemysław Prencel <przemyslaw.prencel@goodylabs.com>
2020-10-08 00:34:45 +0200

    init

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello world!</h1>
+      <p>It's DigitalOcean App Platform - test</p>
+    </div>
+  )
+}
